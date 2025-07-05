69804ef55dcbba22f7499dcd51de9c6213fb0d96
tarun6546 <tarunvarshney2112@gmail.com>
2025-07-05 23:30:12 +0530

    Add initial implementation of linked list with insert and display methods

---
+++ LinkedList/bimplementation.java
@@ -0,0 +1,69 @@
+public class bimplementation {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static class linkedlist{
+        Node head = null;
+        Node tail = null;
+        void insertAtEnd(int data){
+            Node temp = new Node(data);
+            if(head ==null){
+                head = temp;
+                tail = temp;
+            }
+            else{
+                tail.next = temp;
+                
+            }
+            tail = temp;
+
+        }
+        void insertAtHead(int data){
+            Node temp = new Node(data);
+            if(head==null){
+                head=tail=temp;
+            }
+            else{
+                temp.next = head;
+                head = temp;
+            }
+        }
+        void display(){
+            Node temp = head;
+            while(temp!=null){
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+        int size(){
+            int count=0;
+            Node temp=head;
+            while(temp!=null){
+                count++;
+                temp = temp.next;
+            }
+            return count;
+        }
+        
+        
+    }
+    public static void main(String[] args) {
+        linkedlist ll = new linkedlist();
+        ll.insertAtEnd(5);
+        ll.insertAtEnd(10);
+        ll.insertAtEnd(15); 
+        ll.insertAtEnd(20);
+        ll.display();
+        System.out.println("\nSize of the linked list is: " + ll.size() + "\n"  );
+        ll.insertAtHead(2);
+        ll.insertAtHead(1);
+        ll.display();
+    }
+    
+}
