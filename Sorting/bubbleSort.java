dcd28a3f63437109592332b00213aa3890bc4976
tarun6546 <tarunvarshney2112@gmail.com>
2025-07-09 12:36:32 +0530

    Implement bubble sort algorithm with input handling and array display

--- Sorting/bubbleSort.java
+++ Sorting/bubbleSort.java
@@ -1,5 +1,41 @@
 package Sorting;
+import java.util.Scanner;
 
 public class bubbleSort {
+    public static void sort(int[] arr){
+        int n=arr.length;
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+
+                
+            }
+        }
+    }
+    public static void printArray(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("Enter the number of elements in the array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Original array:");
+        printArray(arr);
+        sort(arr);
+        System.out.println("Sorted array:");
+        printArray(arr); 
+    }
 }
