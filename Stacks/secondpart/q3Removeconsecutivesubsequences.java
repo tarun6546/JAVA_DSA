ce7bf8b48c101151955efdb406e80ddbf420790a
tarun6546 <tarunvarshney2112@gmail.com>
2025-07-29 00:12:13 +0530

    Refactor remove method to eliminate consecutive duplicate elements from an array using a stack

--- Stacks/secondpart/q3Removeconsecutivesubsequences.java
+++ Stacks/secondpart/q3Removeconsecutivesubsequences.java
@@ -1,11 +1,36 @@
 package Stacks.secondpart;
 
+import java.util.Stack;
+
 public class q3Removeconsecutivesubsequences {
-    public stati
+
+    public static int[] remove(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!stack.isEmpty() && stack.peek() == arr[i]) {
+                // If current element is same as top of stack, pop it (remove the pair)
+                stack.pop();
+            } else {
+                stack.push(arr[i]);
+            }
+        }
+
+        // Now convert stack to array
+        int[] result = new int[stack.size()];
+        for (int i = result.length - 1; i >= 0; i--) {
+            result[i] = stack.pop();
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] arr = {1, 2, 2, 3, 3, 4, 4, 5};
         int[] result = remove(arr);
-
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
     }
-	
 }
+// This code removes consecutive duplicate elements from an array using a stack.
