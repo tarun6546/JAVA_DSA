3cdff05cdb688560a5a7d4d41d2e1c91d3ee3e3c
tarun6546 <tarunvarshney2112@gmail.com>
2025-09-23 23:53:09 +0530

    remove line of code

--- Questions/Strings/basic/q4.java
+++ Questions/Strings/basic/q4.java
@@ -31,7 +31,7 @@ public class q4 {
         } else {
             System.out.println("No, the strings are NOT Anagram ❌");
         }
-        //close the scanner cla
+        //close the scanner c
         sc.close();
         
 
