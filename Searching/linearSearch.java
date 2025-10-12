02cf36bafa529a67c056a0d04283662adef4c9c5
tarun6546 <tarunvarshney2112@gmail.com>
2025-10-12 23:36:01 +0530

    fix: update prompt for array size input in linearSearch.java

--- Searching/linearSearch.java
+++ Searching/linearSearch.java
@@ -12,7 +12,7 @@ public class linearSearch {
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of the array:");
+        System.out.println("Enter the size of array:");
         int n = sc.nextInt();
         int [] arr = new int[n];
         System.out.println("Enter the elements of the array:");
